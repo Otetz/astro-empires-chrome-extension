504d48a722d3d85413eb4a8c8660e3eb5b471af5
Alexey Shevchenko <otetz@me.com>
2015-02-07 03:03:54 +0200

    Choose server from full list.

--- options/options.js
+++ options/options.js
@@ -1,5 +1,42 @@
 
 
+function populateServers(servers) {
+  var select = window.$('#server');
+  var options;
+  if(select.prop) { options = select.prop('options'); }
+  else { options = select.attr('options'); }
+  window.$.each(servers, function(key, value) {
+    options[options.length] = new Option(value, value);
+  });
+}
+
+function fillServersList() {
+  var dateLastUpdate = new Date(localStorage.serverLastUpdate);
+  var checkingDate = new Date();
+  checkingDate.setDate(checkingDate.getDate() - 1);
+
+  if (dateLastUpdate >= checkingDate) {
+    populateServers(JSON.parse(localStorage.servers));
+    return;
+  }
+  window.$.ajax({
+    url:'https://www.kimonolabs.com/api/7qv8xhz8?apikey=OSyC9dRS8dd380hhhRHmN1MYa2yfuzil&kimmodify=1',
+    success: function (response) {
+      var servers = [];
+      window.$.each(response.results.servers, function(key, value) {
+        servers[servers.length] = value;
+      });
+      localStorage.servers = JSON.stringify(servers);
+      var dateUpdated = new Date();
+      localStorage.serverLastUpdate = dateUpdated.toISOString();
+      populateServers(servers);
+    },
+    error: function (xhr, status) {
+      console.log('Error loading servers list: ' + status);
+    }
+  });
+}
+
 // Saves options to localStorage.
 function saveOptions() {
   localStorage.server = window.$('#server').val();
@@ -7,18 +44,22 @@ function saveOptions() {
   localStorage.password = window.$('#password').val();
 
   // Update status to let user know options were saved.
-  var status = document.getElementById("status");
-  status.innerHTML = "Options Saved.";
+  window.$('#status').html('Options Saved.');
   setTimeout(function() {
-    status.innerHTML = "";
-  }, 750);
+    window.$('#status').html('');
+  }, 1250);
 }
 
 // Restores select box state to saved value from localStorage.
-function restore_options() {
-  jQuery('select#server').val(localStorage['server']);
-  jQuery('input#email').val(localStorage['email']);
-  jQuery('input#password').val(localStorage['password']);
+function restoreOptions() {
+  console.log('Restoring options...');
+
+  fillServersList();
+
+  window.$('#server').val(localStorage.server);
+  window.$('#email').val(localStorage.email);
+  window.$('#password').val(localStorage.password);
 }
-document.addEventListener('DOMContentLoaded', restore_options);
-document.querySelector('#save').addEventListener('click', save_options);
+
+window.$(document).ready(restoreOptions);
+window.$('#save').click(saveOptions);
